6758430aff6ed391962f9184d3f3904f9f45b5ee
coding-clown <iec2018045@gmail.com>
2019-07-06 10:13:25 +0530

    updated eventlisteners

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var budgetController= (function(){
 
 //UI CONTROLLER
 var UIController= (function(){
+   var  DomStrings={
+      inputType:".add__type",
+      inputDes:".add__description",
+      inputNum:".add__value"
+    }
+
    return {
         getInput:function(){
           return{
-            type:document.querySelector(".add__type").value,
-            description:document.querySelector(".add__description").value,
-            number:document.querySelector(".add__value").value
+            type:document.querySelector(DomStrings.inputType).value,
+            description:document.querySelector(DomStrings.inputDes).value,
+            number:document.querySelector(DomStrings.inputNum).value
           };
        
+        },
+        getDomStrings:function(){
+          return DomStrings;
         }
     };
 
@@ -25,18 +34,31 @@ var UIController= (function(){
 
 //GLOBAL APP CONTROLLER
 var controller=(function(budgetctrl,UIctrl){
-
-    var ctrlAdditem= function(){
-      var input=UIctrl.getInput;
-      console.log(input());
-
-    }
-    document.querySelector('.add__btn').addEventListener('click',ctrlAdditem);
+    var setUpEventListeners=function(){
+      document.querySelector('.add__btn').addEventListener('click',ctrlAdditem);
 
     document.addEventListener('keypress',function(e){
       if(e.keyCode==13||e.which==13){
         ctrlAdditem();
       }
     });
+    }
+
+
+    var Dom=UIctrl.getDomStrings;
+    var ctrlAdditem= function(){
+      var input=UIctrl.getInput;
+
+      console.log(input());
+
+    }
+   return {
+     init: function()
+     {
+       setUpEventListeners();
+     }
+   }
+    
 
 })(budgetController,UIController);
+controller.init();
